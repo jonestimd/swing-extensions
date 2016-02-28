814c2517a66771b20130dbf7ce36f333e5778604
Tim Jones <jonestimd@gmail.com>
2016-02-28 14:01:55 -0600

    Add test coverage

--- src/test/java/io/github/jonestimd/swing/table/MixedRowTableTest.java
+++ src/test/java/io/github/jonestimd/swing/table/MixedRowTableTest.java
@@ -1,21 +1,43 @@
 package io.github.jonestimd.swing.table;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
 
+import javax.swing.DefaultCellEditor;
+import javax.swing.JTextField;
+import javax.swing.RowSorter.SortKey;
+import javax.swing.SortOrder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableColumn;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import io.github.jonestimd.swing.ComponentDefaults;
 import io.github.jonestimd.swing.table.model.BufferedHeaderDetailTableModel;
 import io.github.jonestimd.swing.table.model.ColumnAdapter;
+import io.github.jonestimd.swing.table.model.EmptyColumnAdapter;
 import io.github.jonestimd.swing.table.model.SingleTypeDetailAdapter;
+import io.github.jonestimd.swing.table.model.TestColumnAdapter;
+import io.github.jonestimd.swing.table.sort.HeaderDetailTableRowSorter;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static java.util.Collections.*;
+import static org.fest.assertions.Assertions.*;
 
 public class MixedRowTableTest {
+    private Color evenBackground = ComponentDefaults.getColor("Table.alternateRowColor");
+    private Color oddBackground = ComponentDefaults.getColor("Table.background");
+    private final ColumnAdapter<TestSummaryBean, String> summaryColumnAdapter1 =
+            new TestColumnAdapter<>("Summary Name", "summaryName", String.class, TestSummaryBean::getName, TestSummaryBean::setName);
+    private final ColumnAdapter<TestSummaryBean, Integer> summaryColumnAdapter2 =
+            new TestColumnAdapter<>("Summary Number", "summaryNumber", Integer.class, TestSummaryBean::getNumber, TestSummaryBean::setNumber);
+    private final ColumnAdapter<TestDetailBean, Integer> detailColumnAdapter =
+            new TestColumnAdapter<>("Detail Id", "detailId", Integer.class, TestDetailBean::getId, TestDetailBean::setId);
     private int nextId = 1;
 
     @Test
@@ -25,12 +47,12 @@ public class MixedRowTableTest {
 
         table.setModel(model);
 
-        assertEquals(1, table.getColumnModel().getColumnCount());
-        assertEquals("Summary Name", table.getColumnModel().getColumn(0).getHeaderValue());
-        assertEquals("Detail Id", ((MixedRowTableColumn) table.getColumnModel().getColumn(0)).getSubColumn(0).getHeaderValue());
+        assertThat(table.getColumnModel().getColumnCount()).isEqualTo(2);
+        assertThat(table.getColumnModel().getColumn(0).getHeaderValue()).isEqualTo("Summary Name");
+        assertThat(((MixedRowTableColumn) table.getColumnModel().getColumn(0)).getSubColumn(0).getHeaderValue()).isEqualTo("Detail Id");
         MixedRowTableColumn column = (MixedRowTableColumn) table.getColumnModel().getColumn(0);
-        assertEquals("Summary Name", column.getHeaderValue());
-        assertEquals("Detail Id", column.getSubColumn(0).getHeaderValue());
+        assertThat(column.getHeaderValue()).isEqualTo("Summary Name");
+        assertThat(column.getSubColumn(0).getHeaderValue()).isEqualTo("Detail Id");
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -46,12 +68,12 @@ public class MixedRowTableTest {
 
         MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
 
-        assertEquals(1, table.getColumnModel().getColumnCount());
-        assertEquals("<html><center>Summary Name</center></html>", table.getColumnModel().getColumn(0).getHeaderValue());
-        assertEquals("Detail Id", ((MixedRowTableColumn) table.getColumnModel().getColumn(0)).getSubColumn(0).getHeaderValue());
+        assertThat(table.getColumnModel().getColumnCount()).isEqualTo(2);
+        assertThat(table.getColumnModel().getColumn(0).getHeaderValue()).isEqualTo("<html><center>Summary Name</center></html>");
+        assertThat(((MixedRowTableColumn) table.getColumnModel().getColumn(0)).getSubColumn(0).getHeaderValue()).isEqualTo("Detail Id");
         MixedRowTableColumn column = (MixedRowTableColumn) table.getColumnModel().getColumn(0);
-        assertEquals("<html><center>Summary Name</center></html>", column.getHeaderValue());
-        assertEquals("Detail Id", column.getSubColumn(0).getHeaderValue());
+        assertThat(column.getHeaderValue()).isEqualTo("<html><center>Summary Name</center></html>");
+        assertThat(column.getSubColumn(0).getHeaderValue()).isEqualTo("Detail Id");
     }
 
     @Test
@@ -63,117 +85,210 @@ public class MixedRowTableTest {
         column.setIdentifier("new column");
         table.addColumn(column);
 
-        assertEquals(2, table.getColumnModel().getColumnCount());
+        assertThat(table.getColumnModel().getColumnCount()).isEqualTo(3);
         Enumeration<TableColumn> columns = table.getColumnModel().getColumns();
         while (columns.hasMoreElements()) {
-            assertEquals(MixedRowTableColumn.class, columns.nextElement().getClass());
+            assertThat(columns.nextElement().getClass()).isEqualTo(MixedRowTableColumn.class);
         }
     }
 
     @Test
-    public void testGetCellEditor() throws Exception {
+    public void getCellEditorUsesDefaultEditor() throws Exception {
         BufferedHeaderDetailTableModel<TestSummaryBean> model = newModel();
-        model.setBeans(Arrays.asList(new TestSummaryBean(new TestDetailBean(), new TestDetailBean())));
+        model.setBeans(singletonList(new TestSummaryBean(new TestDetailBean(), new TestDetailBean())));
         MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
 
-        assertEquals("io.github.jonestimd.swing.table.MixedRowTable$GenericCellEditor", table.getCellEditor(0, 0).getClass().getName());
-        assertEquals("io.github.jonestimd.swing.table.MixedRowTable$NumberCellEditor", table.getCellEditor(1, 0).getClass().getName());
-        assertEquals("io.github.jonestimd.swing.table.MixedRowTable$NumberCellEditor", table.getCellEditor(2, 0).getClass().getName());
+        assertThat(table.getCellEditor(0, 0).getClass().getName()).isEqualTo("io.github.jonestimd.swing.table.MixedRowTable$GenericCellEditor");
+        assertThat(table.getCellEditor(1, 0).getClass().getName()).isEqualTo("io.github.jonestimd.swing.table.MixedRowTable$NumberCellEditor");
+        assertThat(table.getCellEditor(2, 0).getClass().getName()).isEqualTo("io.github.jonestimd.swing.table.MixedRowTable$NumberCellEditor");
     }
 
     @Test
-    public void testGetCellRenderer() throws Exception {
+    public void getCellEditorUsesColumnEditor() throws Exception {
         BufferedHeaderDetailTableModel<TestSummaryBean> model = newModel();
-        model.setBeans(Arrays.asList(new TestSummaryBean(new TestDetailBean(), new TestDetailBean())));
+        model.setBeans(singletonList(new TestSummaryBean(new TestDetailBean(), new TestDetailBean())));
+        MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
+        MixedRowTableColumn column = (MixedRowTableColumn) table.getColumnModel().getColumn(0);
+        final DefaultCellEditor cellEditor = new DefaultCellEditor(new JTextField());
+        column.getSubColumn(0).setCellEditor(cellEditor);
+
+        assertThat(table.getCellEditor(0, 0).getClass().getName()).isEqualTo("io.github.jonestimd.swing.table.MixedRowTable$GenericCellEditor");
+        assertThat(table.getCellEditor(1, 0)).isSameAs(cellEditor);
+        assertThat(table.getCellEditor(2, 0)).isSameAs(cellEditor);
+    }
+
+    @Test
+    public void getCellRendererUsesDefaultRenderer() throws Exception {
+        BufferedHeaderDetailTableModel<TestSummaryBean> model = newModel();
+        model.setBeans(singletonList(new TestSummaryBean(new TestDetailBean(), new TestDetailBean())));
+        MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
+
+        assertThat(table.getCellRenderer(0, 0)).isSameAs(table.getDefaultRenderer(String.class));
+        assertThat(table.getCellRenderer(1, 0)).isSameAs(table.getDefaultRenderer(Integer.class));
+        assertThat(table.getCellRenderer(2, 0)).isSameAs(table.getDefaultRenderer(Integer.class));
+    }
+
+    @Test
+    public void getCellRendererUsesColumnRenderer() throws Exception {
+        BufferedHeaderDetailTableModel<TestSummaryBean> model = newModel();
+        model.setBeans(singletonList(new TestSummaryBean(new TestDetailBean(), new TestDetailBean())));
         MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
         DefaultTableCellRenderer renderer1 = new DefaultTableCellRenderer();
         DefaultTableCellRenderer renderer2 = new DefaultTableCellRenderer();
         table.getColumnModel().getColumn(0).setCellRenderer(renderer1);
         ((MixedRowTableColumn) table.getColumnModel().getColumn(0)).getSubColumn(0).setCellRenderer(renderer2);
 
-        assertSame(renderer1, table.getCellRenderer(0, 0));
-        assertSame(renderer2, table.getCellRenderer(1, 0));
-        assertSame(renderer2, table.getCellRenderer(2, 0));
+        assertThat(table.getCellRenderer(0, 0)).isSameAs(renderer1);
+        assertThat(table.getCellRenderer(1, 0)).isSameAs(renderer2);
+        assertThat(table.getCellRenderer(2, 0)).isSameAs(renderer2);
+    }
+
+    @Test
+    public void getRowBackgroundWithoutSorter() throws Exception {
+        BufferedHeaderDetailTableModel<TestSummaryBean> model = newModel();
+        model.setBeans(Lists.newArrayList(
+                new TestSummaryBean(new TestDetailBean()),
+                new TestSummaryBean(new TestDetailBean(), new TestDetailBean())));
+        MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
+
+        assertThat(table.getRowBackground(0)).isEqualTo(evenBackground);
+        assertThat(table.getRowBackground(1)).isEqualTo(evenBackground);
+        assertThat(table.getRowBackground(2)).isEqualTo(oddBackground);
+        assertThat(table.getRowBackground(3)).isEqualTo(oddBackground);
+        assertThat(table.getRowBackground(4)).isEqualTo(oddBackground);
+    }
+
+    @Test
+    public void getRowBackgroundWithSorter() throws Exception {
+        BufferedHeaderDetailTableModel<TestSummaryBean> model = newModel();
+        model.setBeans(Lists.newArrayList(
+                new TestSummaryBean(new TestDetailBean()),
+                new TestSummaryBean(new TestDetailBean(), new TestDetailBean())));
+        MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
+        HeaderDetailTableRowSorter<?, ?> sorter = new HeaderDetailTableRowSorter<>(table);
+        sorter.setSortKeys(Lists.newArrayList(new SortKey(0, SortOrder.DESCENDING)));
+        table.setRowSorter(sorter);
+
+        assertThat(table.getRowBackground(0)).isEqualTo(evenBackground);
+        assertThat(table.getRowBackground(1)).isEqualTo(evenBackground);
+        assertThat(table.getRowBackground(2)).isEqualTo(evenBackground);
+        assertThat(table.getRowBackground(3)).isEqualTo(oddBackground);
+        assertThat(table.getRowBackground(4)).isEqualTo(oddBackground);
+    }
+
+    @Test
+    public void selectRowAt() throws Exception {
+        BufferedHeaderDetailTableModel<TestSummaryBean> model = newModel();
+        model.setBeans(Lists.newArrayList(new TestSummaryBean(new TestDetailBean())));
+        MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
+        table.setRowSelectionInterval(0, 0);
+        table.setColumnSelectionInterval(1, 1);
+
+        table.selectRowAt(1);
+
+        assertThat(table.getSelectedRow()).isEqualTo(1);
+        assertThat(table.getSelectedColumn()).isEqualTo(0);
+        assertThat(table.getLeadSelectionModelIndex()).isEqualTo(1);
+    }
+
+    @Test
+    public void editSummaryCell() throws Exception {
+        BufferedHeaderDetailTableModel<TestSummaryBean> model = newModel();
+        model.setBeans(Lists.newArrayList(new TestSummaryBean(new TestDetailBean())));
+        MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
+        final TableCellEditor editor = table.getDefaultEditor(String.class);
+
+        Component component = editor.getTableCellEditorComponent(table, "", false, 0, 0);
+        ((JTextField) component).setText("new value");
+        assertThat(editor.stopCellEditing()).isTrue();
+
+        assertThat(editor.getCellEditorValue()).isEqualTo("new value");
+    }
+
+    @Test
+    public void editSummaryCellEmptyString() throws Exception {
+        BufferedHeaderDetailTableModel<TestSummaryBean> model = newModel();
+        model.setBeans(Lists.newArrayList(new TestSummaryBean(new TestDetailBean())));
+        MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
+        final TableCellEditor editor = table.getDefaultEditor(String.class);
+
+        Component component = editor.getTableCellEditorComponent(table, "old value", false, 0, 0);
+        ((JTextField) component).setText("");
+        assertThat(editor.stopCellEditing()).isTrue();
+
+        assertThat(editor.getCellEditorValue()).isEqualTo("");
+    }
+
+    @Test
+    public void editSubRowCell() throws Exception {
+        BufferedHeaderDetailTableModel<TestSummaryBean> model = newModel();
+        model.setBeans(Lists.newArrayList(new TestSummaryBean(new TestDetailBean())));
+        MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
+        final TableCellEditor editor = table.getDefaultEditor(String.class);
+
+        Component component = editor.getTableCellEditorComponent(table, "", false, 1, 0);
+        ((JTextField) component).setText("99");
+        assertThat(editor.stopCellEditing()).isTrue();
+
+        assertThat(editor.getCellEditorValue()).isEqualTo(99);
+    }
+
+    @Test
+    public void editSubRowCellInvalidValue() throws Exception {
+        BufferedHeaderDetailTableModel<TestSummaryBean> model = newModel();
+        model.setBeans(Lists.newArrayList(new TestSummaryBean(new TestDetailBean())));
+        MixedRowTable<TestSummaryBean, BufferedHeaderDetailTableModel<TestSummaryBean>> table = new MixedRowTable<>(model);
+        final TableCellEditor editor = table.getDefaultEditor(String.class);
+
+        Component component = editor.getTableCellEditorComponent(table, "", false, 1, 0);
+        ((JTextField) component).setText("");
+        assertThat(editor.stopCellEditing()).isFalse();
+
+        assertThat(editor.getCellEditorValue()).isNull();
     }
 
     @SuppressWarnings("unchecked")
     private BufferedHeaderDetailTableModel<TestSummaryBean> newModel() {
         return new BufferedHeaderDetailTableModel<>(new TestDetailAdapter(),
-            Arrays.asList(new TestSummaryColumnAdapter()),
-            Arrays.asList(Arrays.asList(new TestDetailColumnAdapter())));
+                ImmutableList.of(summaryColumnAdapter1, summaryColumnAdapter2),
+                singletonList(ImmutableList.of(detailColumnAdapter, new EmptyColumnAdapter<>("dummy1"))));
     }
 
     private class TestDetailBean {
         private Integer id = nextId++;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
     }
 
     private class TestSummaryBean {
         private List<TestDetailBean> details;
         private String name = "summary " + nextId++;
-        private boolean editable = false;
+        private Integer number;
 
         private TestSummaryBean(TestDetailBean ... details) {
             this.details = Arrays.asList(details);
         }
-    }
-
-    private class TestDetailColumnAdapter implements ColumnAdapter<TestDetailBean, Integer> {
-        public String getColumnId() {
-            return "detailId";
-        }
-
-        public String getResource(String columnId, String defaultValue) {
-            return null;
-        }
 
         public String getName() {
-            return "Detail Id";
+            return name;
         }
 
-        public Class<Integer> getType() {
-            return Integer.class;
+        public void setName(String name) {
+            this.name = name;
         }
 
-        public Integer getValue(TestDetailBean row) {
-            return row.id;
+        public Integer getNumber() {
+            return number;
         }
 
-        public boolean isEditable(TestDetailBean row) {
-            return true;
-        }
-
-        public void setValue(TestDetailBean row, Integer value) {
-            row.id = value;
-        }
-    }
-
-    private class TestSummaryColumnAdapter implements ColumnAdapter<TestSummaryBean, String> {
-        public String getColumnId() {
-            return "summaryName";
-        }
-
-        public String getResource(String resourceId, String defaultValue) {
-            return null;
-        }
-
-        public String getName() {
-            return "Summary Name";
-        }
-
-        public Class<String> getType() {
-            return String.class;
-        }
-
-        public String getValue(TestSummaryBean row) {
-            return row.name;
-        }
-
-        public boolean isEditable(TestSummaryBean row) {
-            return row.editable;
-        }
-
-        public void setValue(TestSummaryBean row, String value) {
-            row.name = value;
+        public void setNumber(Integer number) {
+            this.number = number;
         }
     }
 
