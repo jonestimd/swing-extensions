9ce2ef10791cedc8b9baf632cc4432c421d3ac2a
Tim Jones <jonestimd@gmail.com>
2020-09-27 09:15:57 -0500

    Support changing the ComboBox editor

--- src/main/java/io/github/jonestimd/swing/component/SuggestField.java
+++ src/main/java/io/github/jonestimd/swing/component/SuggestField.java
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2017 Timothy D. Jones
+// Copyright (c) 2020 Timothy D. Jones
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -47,6 +47,13 @@ public class SuggestField<T> extends BeanListComboBox<T> {
 
     public SuggestField(Format format, Format itemFormat, Validator<String> validator, SuggestModel<T> model) {
         super(format, itemFormat, validator, model);
+        initEditor();
+        addPropertyChangeListener("editor", (event) -> {
+            if (event.getNewValue() != null) initEditor();
+        });
+    }
+
+    private void initEditor() {
         getEditorComponent().addKeyListener(new KeyAdapter() {
             @Override
             public void keyReleased(KeyEvent event) {
